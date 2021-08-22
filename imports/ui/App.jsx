cfb9b6753ce6f5a6b090f5386a44124eaeabf3ec
krae <kraewind@gmail.com>
2021-08-22 11:31:57 -0400

    deployed

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,9 +8,7 @@ import { LoginForm } from './LoginForm';
 
 export const App = () => {
   
-
   const user = useTracker(() => Meteor.user());
-
   
   const [hideCompleted, setHideCompleted] = useState(false);
   
@@ -20,26 +18,47 @@ export const App = () => {
 
   const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
   
-  const tasks = useTracker(() => {
-    if (!user) {
-      return [];
+  const { tasks, pendingTasksCount, isLoading } = useTracker(() => {
+    const noDataAvailable = { tasks: [], pendingTasksCount: 0 };
+    if (!Meteor.user()) {
+      return noDataAvailable;
     }
-    
-    return TasksCollection.find(
+    const handler = Meteor.subscribe('tasks');
+
+    if (!handler.ready()) {
+      return { ...noDataAvailable, isLoading: true };
+    }
+
+    const tasks = TasksCollection.find(
       hideCompleted ? pendingOnlyFilter : userFilter,
       {
         sort: { createdAt: -1 },
       }
-      ).fetch();
-    });
-    
-  const pendingTasksCount = useTracker(() => {
-    if (!user) {
-      return 0;
-    }
+    ).fetch();
+    const pendingTasksCount = TasksCollection.find(pendingOnlyFilter).count();
 
-    return TasksCollection.find(pendingOnlyFilter).count();
+    return { tasks, pendingTasksCount };
   });
+  // const tasks = useTracker(() => {
+  //   if (!user) {
+  //     return [];
+  //   }
+    
+  //   return TasksCollection.find(
+  //     hideCompleted ? pendingOnlyFilter : userFilter,
+  //     {
+  //       sort: { createdAt: -1 },
+  //     }
+  //     ).fetch();
+  //   });
+    
+  // const pendingTasksCount = useTracker(() => {
+  //   if (!user) {
+  //     return 0;
+  //   }
+
+  //   return TasksCollection.find(pendingOnlyFilter).count();
+  // });
 
   const pendingTasksTitle = `${
     pendingTasksCount ? ` (${pendingTasksCount})` : ''
@@ -51,6 +70,8 @@ export const App = () => {
   const deleteTask = ({ _id }) => Meteor.call('tasks.remove', _id);
 
   const logout = () => Meteor.logout();
+
+
 
 
   return (
@@ -79,6 +100,8 @@ export const App = () => {
                 {hideCompleted ? 'Show All' : 'Hide Completed'}
               </button>
             </div>
+
+            {isLoading && <div className="loading">loading...</div>}
 
             <ul className="tasks">
               {tasks.map(task => (
--- tests/main.js
+++ tests/main.js
@@ -1,4 +1,6 @@
 import assert from "assert";
+import '/imports/api/tasksMethods.tests.js';
+
 
 describe("todos-meteor-react", function () {
   it("package.json has correct name", async function () {
